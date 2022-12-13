408ea634f3fc978853d7b90908ada9379dabf793
jaami parvez <jaami.parvez@gmail.com>
2022-12-13 13:00:56 -0500

    use Table component in Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Table from "../components/Table"
 
 type Props = {}
 
 const Dashboard = (props: Props) => {
   return (
-    <div>Dashboard</div>
+    <div className="flex justify-center flex-col w-full p-20 h-[]">
+    <h1 className="flex justify-center">Dashboard</h1>
+   <Table/>
+    </div>
   )
 }
 
