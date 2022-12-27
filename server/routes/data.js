e41b22bb6eb61dfaa0b5cb7d40e3e422d6325c0a
jaami parvez <jaami.parvez@gmail.com>
2022-12-27 12:14:15 -0500

    Change models name

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const DataModel = require('../models/DataModel')
+const DataModel = require('../models/data')
 
 router.get('/',async (req,res)=>{
     const data = await DataModel.find()
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const UserModel = require('../models/UserModel')
+const UserModel = require('../models/user')
 
 
 router.post('/',async (req,res)=>{
